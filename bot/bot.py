bdb5f3de994434e81082c8963886ed2dea795bdd
lukaszmalyszko <lukaszz.malyszko@gmail.com>
2018-08-22 11:26:57 +0200

    bot

---
+++ bot/bot.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import random
+
+# Sentences we'll respond with if the user greeted us
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+
+GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
+
+app = Flask(__name__)
+
+@app.route('/get_sentence', methods=['POST'])
+def get_sentence():
+    word = request.get_json(silent=True)
+    if word['message'] in GREETING_KEYWORDS:
+        return jsonify(random.choice(GREETING_RESPONSES))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
