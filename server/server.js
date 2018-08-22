51af95a62075ccfe5ee339fbae05c5a9716d29d3
alek <alek1296@gmail.com>
2018-08-22 20:54:12 +0200

    http request using request package

--- server/server.js
+++ server/server.js
@@ -1,57 +1,22 @@
 var http = require('http');
 var url = require('url');
-
-var headers = {
-    'User-Agent':       'Super Agent/0.0.1',
-    'Content-Type':     'application/x-www-form-urlencoded'
-}
+var _request = require('request');
 
 var options = {
-    host: '',
-    path: '',
-    headers: headers
+  json: true,
+  url: 'https://jsonplaceholder.typicode.com/posts',
 }
 
-function jsonToQueryString(json) {
-    return '?' + 
-        Object.keys(json).map(function(key) {
-            return encodeURIComponent(key) + '=' +
-                encodeURIComponent(json[key]);
-        }).join('&');
-}
-
-function getMessage(callback) { 
-	return http.get(options, function(response) {
-	  // Continuously update stream with data
-    var body = '';
-    response.on('data', function(d) {
-        body += d;
-    });
-    response.on('end', function() {
-
-        // Data reception is done, do whatever with it!
-        var parsed = JSON.parse(body);
-        callback({
-            message: parsed
-        });
-    }).on("error", (err) => {
-	  console.log("Error: " + err.message);
-	});
-});
-
-}
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
   
   var query = url.parse(request.url, true).query;
-  query = jsonToQueryString(query);
-  var path = options['path'];
-  options['path'] += query;
-  getMessage(function(callback) {
-		response.end(JSON.stringify(callback))
-		});
-  options['path'] = path;
+  options['qs'] = query;
+  _request(options, (err, res, body) => {
+		if (err) { return console.log(err); }
+		response.end(JSON.stringify(body));
+	});
 };
 
 var www = http.createServer(handleRequest);
