d0bcd5d7ab2e043d1f58d1ffd894ca7e3e755e48
lukaszmalyszko <lukaszz.malyszko@gmail.com>
2018-08-23 14:14:44 +0200

    add response to bot

--- bot/bot.py
+++ bot/bot.py
@@ -4,7 +4,7 @@ import random
 # Sentences we'll respond with if the user greeted us
 GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
 
-GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
+GREETING_RESPONSES = ["sup bro", "hey", "*nods*", "hey you get my snap?"]
 
 app = Flask(__name__)
 
@@ -13,6 +13,8 @@ def get_sentence():
     word = request.args.get('message')
     if word in GREETING_KEYWORDS:
         return jsonify(random.choice(GREETING_RESPONSES))
+    else: 
+	return jsonify("man, i can't understand you")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
