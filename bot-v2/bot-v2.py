2c275fc202994da52443e028ee528b6be4742df9
alekzizz <alekziuzia@gmail.com>
2018-09-11 12:03:05 +0200

    retries policy

--- bot-v2/bot-v2.py
+++ bot-v2/bot-v2.py
@@ -1,23 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 import random, logging
-
+ 
 # Sentences we'll respond with if the user greeted us
 GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
-
+ 
 GREETING_RESPONSES = ["sup bro :)", "hey :)", "*nods* :)", "hey you get my snap? :)"]
-
+ 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler())
 app.logger.setLevel(logging.INFO)
-
+ 
 @app.route('/get_sentence', methods=['GET'])
 def get_sentence():
     word = request.args.get('message')
     if word in GREETING_KEYWORDS:
         return jsonify(random.choice(GREETING_RESPONSES))
-    else: 
-	app.logger.warning('Unable to send response')
-	return jsonify("man, i can't understand you :(")
-
+    else:
+        if 'fail' in request.headers:
+            if float(request.headers['fail']) >= round(random.random(),1):
+                abort(500) 
+        app.logger.warning('Unable to send response')
+        return jsonify("man, i can't understand you :(")
+ 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
