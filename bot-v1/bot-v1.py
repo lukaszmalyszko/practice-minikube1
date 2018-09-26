0e76febb6cbaf0a52742a5fb864bf47c7328244f
alekzizz <alekziuzia@gmail.com>
2018-09-26 09:36:12 +0200

    fix config map for prometheus

--- bot-v1/bot-v1.py
+++ bot-v1/bot-v1.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import random, logging
 
-# Sentences we'll respond with if the user greeted us
 GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
 
 GREETING_RESPONSES = ["sup bro", "hey", "*nods*", "hey you get my snap?"]
